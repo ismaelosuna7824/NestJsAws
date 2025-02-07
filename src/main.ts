deab43c50e9cfbf4679700381dca525068ed6dfa
ismael osuna <48770262+ismaelosuna7824@users.noreply.github.com>
2025-02-07 02:11:48 -0700

    chore: change port app

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
-
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 8080;
+  await app.listen(port);
 }
 bootstrap();
