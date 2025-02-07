7e21def2c27005d3214c64bd466565a0a2c78330
ismael osuna <48770262+ismaelosuna7824@users.noreply.github.com>
2025-02-07 15:11:51 -0700

    fix: prettier

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('OpenApi')
-    .setDescription('The cast Api description')
-    .setVersion('1.0')
-    .addTag('cats')
+    .setTitle("OpenApi")
+    .setDescription("The cast Api description")
+    .setVersion("1.0")
+    .addTag("cats")
     .build();
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup("api", app, documentFactory);
   const port = process.env.PORT || 8080;
   await app.listen(port);
 }
