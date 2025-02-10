04a4f8fe052b1e5976692f80e9e9b4aabf2353c1
ismael osuna <48770262+ismaelosuna7824@users.noreply.github.com>
2025-02-10 12:26:18 -0700

    chore: add new response service

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): any {
-    return { Hello: 'Hello World Aws' };
+    return { Hello: 'Hello World Aws with cicd' };
   }
 }
